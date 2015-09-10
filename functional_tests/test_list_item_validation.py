f73eb1481c7935f229cf1ea4321352d23add7a49
Huayang WANG <sheepou@hotmail.com>
2015-09-10 13:23:02 +0800

    Use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -22,7 +22,7 @@ class ItemValidationTest(FunctionalTest):
 		self.check_for_row_in_list_table('1: Buy milk')
 		
 		# Perversely, she now decides to submit a second blank list item
-		self.browser.get_item_input_box().send_keys('\n')
+		self.get_item_input_box().send_keys('\n')
 		
 		# She receives a similar warning on the list page
 		self.check_for_row_in_list_table('1: Buy milk')
