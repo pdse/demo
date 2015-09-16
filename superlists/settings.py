f64dca9b31d8ac059d71bc94812d38d9d3a57608
Huayang WANG <sheepou@hotmail.com>
2015-09-16 11:30:06 +0800

    Account created

--- superlists/settings.py
+++ superlists/settings.py
@@ -83,6 +83,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATICFILES_DIRS =(
+	os.path.join(BASE_DIR, 'superlists', 'static'),
+)
 
 # Setting for LiveTestServer
 # https://docs.djangoproject.com/en/1.7/topics/testing/tools/#liveservertestcase
