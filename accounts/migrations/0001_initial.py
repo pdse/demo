8bc6041ef433c2542653335a641153c29d3a53c4
Huayang WANG <sheepou@hotmail.com>
2015-09-17 16:30:19 +0800

    test_authentication finished, starting test_models

---
+++ accounts/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('email', models.EmailField(max_length=75)),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
